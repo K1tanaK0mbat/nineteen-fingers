6f8a14a4e77e33d57be4c3c6f289b25d58c6c37d
K1tanaK0mbat <kkeayouth@gmail.com>
2023-12-13 20:03:06 -0600

    deleted comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,9 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-
-// TODO: Add logic for a method that gets all the content from the database
 
 export const putDb = async (content) => {
   console.log('PUT to the database', content);
